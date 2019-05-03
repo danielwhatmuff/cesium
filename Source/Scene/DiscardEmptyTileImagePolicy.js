c3e60fa3d6736f302734595ed5159d2e5d9af3e6
Scott Hunter <shunter@agi.com>
2019-05-03 11:53:47 -0400

    Use DiscardEmptyTileImagePolicy for all Bing styles.
    This removes the need to inspect pixel values to detect the placeholder "missing tile" image.
    
    Also change `DiscardEmptyTileImagePolicy.EMPTY_IMAGE` to be a real (blank) image.
    This way we always satisfy the documented API of `requestImage`.
    
    Fixes #1353.

--- Source/Scene/DiscardEmptyTileImagePolicy.js
+++ Source/Scene/DiscardEmptyTileImagePolicy.js
@@ -8,6 +8,8 @@ define([
 
     /**
      * A policy for discarding tile images that contain no data (and so aren't actually images).
+     * This policy discards {@link DiscardEmptyTileImagePolicy.EMPTY_IMAGE}, which is
+     * expected to be used in place of any empty tile images by the image loading code.
      *
      * @alias DiscardEmptyTileImagePolicy
      * @constructor
@@ -38,7 +40,9 @@ define([
     /**
      * Default value for representing an empty image.
      */
-    DiscardEmptyTileImagePolicy.EMPTY_IMAGE = {};
+    DiscardEmptyTileImagePolicy.EMPTY_IMAGE = new Image();
+    // load a blank data URI with a 1x1 transparent pixel.
+    DiscardEmptyTileImagePolicy.EMPTY_IMAGE.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
 
     return DiscardEmptyTileImagePolicy;
 });
